e0ae698fc7afc208a91442df11d95516e919c291
Adam D <adamd1@gmail.com>
2015-10-03 17:11:29 -0400

    External google analytics removal
    See above

--- index.php
+++ index.php
@@ -8,18 +8,6 @@
 <link rel="stylesheet" type="text/css" href="css/epj.css" />
 <style type="text/css" media="all">
 </style>
-<!-- begin auto analytics block -->
-<script type="text/javascript">
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-192935-6']);
-_gaq.push(['_trackPageview']);
-(function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-</script>
-<!-- end auto analytics block -->
 </head>
 <body>
 <div class="contain">
