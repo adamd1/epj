01b5bf69e8d9064da4c4f08e172fb42809660123
Adam <adam.drake@corusent.com>
2015-12-10 14:26:05 -0500

    xmas

--- index.php
+++ index.php
@@ -5,30 +5,16 @@
 	<title>> > > > EmergencyPuffJacket.com > > > ></title>
 	<meta name="description" content="EmergencyPuffJacket - doing weird web things since 1999">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href="lib/normalize.css" rel="stylesheet" media="all">
-	<link href="styles.css" rel="stylesheet" media="all">
-	<link href='http://fonts.googleapis.com/css?family=BenchNine:700' rel='stylesheet' type='text/css'>
-	<link href='https://fonts.googleapis.com/css?family=Oswald:700' rel='stylesheet' type='text/css'>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script src="birdman.js"></script>
+	<script src="js/jquery-2.1.0.js"></script>
+  <link rel="stylesheet" href="css/xmasstyle.css"></link>
 </head>
-<body>
-	<header role="banner">
-		<h1 class="Droid-Sans-Normal-100">EmergencyPuffJacket.com</h1>
-	</header>
-	<div class="wrap">
-		<main role="main">
-		<p class="birdman">EmergencyPuffJacket is the portfolio site of Adam Drake, and also a place where he tries a few web experiments.</p>
-		<p class="birdman">It was created in 1999.</p>
-		<p class="birdman">Stay tuned for more experiments.</p>
-		</main>
-	</div>
-	<footer role="contentinfo">
-		<p>small>This site uses the Birdman plugin, created by <a href="https://github.com/chrisma/birdman.js" target="_blank">Christoph Matthies</a></small><br />
-		Visit Adam's main website <a href="http://brainrub.com/adamd/">here</a>.</p>
-	</footer>
-	<script type="text/javascript">
-		$('.birdman').birdman({speedUp:false});
-	</script>
-</body>
+	<body class="xmasBg">
+	<script src='js/snowfall.jquery.min.js'></script>
+	<div class="banner">EmergencyPuffJacket.com</div>
+		<script type='text/javascript'>  
+			$(document).ready(function(){
+				$(document).snowfall();
+			});
+		</script>
+	</body>
 </html>
