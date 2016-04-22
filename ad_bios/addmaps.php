84c49e4b7e4ae8fa025bcd8953c4fd7c603e2a01
Adam <adam.drake@corusent.com>
2016-04-22 15:55:01 -0400

    Some more fixes and updates.

--- ad_bios/addmaps.php
+++ ad_bios/addmaps.php
@@ -6,44 +6,55 @@
    October, 2002
    Adam D.
 */
-// All the basics
-   include_once("elements/basics.epj");
+// require_once __DIR__."/lib/KLogger.php";  // Include KLogger first so config sets log dir
+// Database connection object (Which auto-loads all the site configuration.)
+require_once __DIR__."/data/DBConnector.php";
 
+
+$p_addid="";
+	if(isset($_REQUEST["p_addid"]) && $_REQUEST["p_addid"]!=""){
+		$p_addid=$_REQUEST["p_addid"];
+	}
 // 1st: set defaults for blanks
-      IF(!$p_addid){
-         $p_addid='1';
-      }
-   $add_query="SELECT DISTINCT a.map_url,a.street,
-                               a.city,a.stprv,a.country
+	if($p_addid==""){
+		$p_addid="1";
+	}
+   $add_query="SELECT DISTINCT a.map_url,a.street,a.city,a.stprv,a.country
                FROM my_addresses a
                WHERE (a.add_id = $p_addid)
                LIMIT 1;";
 //   print($add_query);
-// Connect to the database
-   $db = mysql_connect($dbhost,$dbuser,$dbpass);
-   mysql_select_db($dbname,$db);
+// connect to the database
+$db = dbconnector::connect();
 // Now: Run the query for the graph.
-// Run the query for the graph.
-   $add_result=mysql_query($add_query,$db);
-      IF($add_row=mysql_fetch_array($add_result)){
-         $addr_output=str_replace("<br />", " - ", $add_row["street"]).", ";
-         $addr_output.=$add_row["city"].", ".$add_row["stprv"].", ".$add_row["country"];
-// No css this time...
-         $css='music.css';
-         $pagetitle=str_replace("<br />", " - ", $add_row["street"]).", ".$add_row["city"];
-// default html header with comments
-         include("elements/header.epj");
-         print("<body>\n");
-         print("<div style=\"position:absolute;top:0px;left:0px;\" align=\"center\">\n");
-         print("<a href=\"javascript:self.close();\">");
-         printf("<img src=\"%s\" ",$add_row["map_url"]);
-         print("width=\"356\" height=\"249\" border=\"0\" "); 
-         printf(" alt=\"%s\"></a>",$addr_output);
-//         print("<br />\n<a href=\"javascript:self.close();\">close window</a><br />\n");
-         print("</div>\n");
-         print("</body>\n");
-         print("</html>\n");
-      }
-   mysql_free_result($add_result);
-   mysql_close($db);
+// Prepare
+$stmt = $db->prepare($sql);
+// Execute
+$retVal = $stmt -> execute();
+// Row count...
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$rs1 = $db->query('SELECT FOUND_ROWS()');
+$row_count = (int) $rs1->fetchColumn();
+// If row count > 0, do stuff...
+$stmt = null;
+$ctr=0;
+// Process...
+	if($row_count>0){
+		foreach($rows as $row) {
+		$addr_output=str_replace("<br />", " - ", $row["street"]).", ";
+		$addr_output.=$row["city"].", ".$row["stprv"].", ".$row["country"];
+		// No css this time...
+		$css='music.css';
+		$pagetitle=str_replace("<br />", " - ", $row["street"]).", ".$row["city"];
+		// default html header with comments
+		include("elements/header.php");
+		print("<body>\n");
+		print("<div style=\"position:absolute;top:0px;left:0px;\" align=\"center\">\n");
+		print("<a href=\"javascript:self.close();\">");
+		printf("<img src=\"%s\" width=\"356\" height=\"249\" border=\"0\" alt=\"%s\"></a>",$row["map_url"],$addr_output);
+		// print("<br />\n<a href=\"javascript:self.close();\">close window</a><br />\n");
+		print("</div>\n");
+		print("</body>\n");
+		print("</html>\n");
+	}
 ?>
