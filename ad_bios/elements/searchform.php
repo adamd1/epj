1c9760f631faadb49caa678f8be4bd9ae1bcabe4
Adam <adam.drake@corusent.com>
2016-05-03 09:54:54 -0400

    FIxing broken old PHP

--- ad_bios/elements/searchform.php
+++ ad_bios/elements/searchform.php
@@ -1,14 +1,15 @@
 <?php
 /*
-   searchform.epj
+   searchform.php
    A basic one line search field
    Coded Sep. 24th, 2002.
    (c) 2002 Adam Drake
    adam@brainrub.com
 */
-   print("   <form action=\"biosearch.epj\" method=\"post\">\n");
+$p_term=$_REQUEST["$p_term"];
+   print("   <form action=\"biosearch.php\" method=\"post\">\n");
    print("   <input type=\"text\" name=\"p_term\" size=\"20\" ");
-      IF(isset($p_term)){
+      if(isset($p_term)){
          printf("value=\"%s\"",$p_term);
       }
    print(">&nbsp;\n");
